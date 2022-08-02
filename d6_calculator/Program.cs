78e559c3c270df72addd3f2df449a3f54280df7a
Hikanyan <hikanyan0131@gmail.com>
2022-08-02 22:32:51 +0900

    プロジェクト ファイルを追加します。

---
+++ d6_calculator/Program.cs
@@ -0,0 +1,11 @@
+﻿// https://paiza.jp/works/mondai/drankfast/d6_calculator
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        var line = Console.ReadLine();
+        Console.WriteLine("XXXXXX");
+    }
+}
